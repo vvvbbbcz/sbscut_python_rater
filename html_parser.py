01290d0b4e4bf532dd941cb357e1d2e01b0e88ef
vvvbbbcz <vvvbbbcz@bzgzs.cn>
2025-04-15 23:21:09 +0800

    chore: replace space with tab

--- html_parser.py
+++ html_parser.py
@@ -2,43 +2,43 @@ import html.parser
 from logger import logger
 
 class HTMLParser(html.parser.HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.view_state = ""
-        self.view_state_generator = ""
-        self.event_validation = ""
+	def __init__(self):
+		super().__init__()
+		self.view_state = ""
+		self.view_state_generator = ""
+		self.event_validation = ""
 
-    def handle_starttag(self, tag, attrs):
-        if tag == "input":
-            tag_attrs = {}
-            for attr in attrs:
-                tag_attrs[attr[0]] = attr[1]
+	def handle_starttag(self, tag, attrs):
+		if tag == "input":
+			tag_attrs = {}
+			for attr in attrs:
+				tag_attrs[attr[0]] = attr[1]
 
-            if tag_attrs["id"] == "__VIEWSTATE":
-                self.view_state = tag_attrs["value"]
-                logger.debug("Get __VIEWSTATE: " + self.view_state)
-                return
+			if tag_attrs["id"] == "__VIEWSTATE":
+				self.view_state = tag_attrs["value"]
+				logger.debug("Get __VIEWSTATE: " + self.view_state)
+				return
 
-            if tag_attrs["id"] == "__VIEWSTATEGENERATOR":
-                self.view_state_generator = tag_attrs["value"]
-                logger.debug("Get __VIEWSTATEGENERATOR: " + self.view_state_generator)
-                return
+			if tag_attrs["id"] == "__VIEWSTATEGENERATOR":
+				self.view_state_generator = tag_attrs["value"]
+				logger.debug("Get __VIEWSTATEGENERATOR: " + self.view_state_generator)
+				return
 
-            if tag_attrs["id"] == "__EVENTVALIDATION":
-                self.event_validation = tag_attrs["value"]
-                logger.debug("Get __EVENTVALIDATION: " + self.event_validation)
-                return
+			if tag_attrs["id"] == "__EVENTVALIDATION":
+				self.event_validation = tag_attrs["value"]
+				logger.debug("Get __EVENTVALIDATION: " + self.event_validation)
+				return
 
-    def reset(self):
-        super().reset()
-        self.view_state = ""
-        self.view_state_generator = ""
-        self.event_validation = ""
+	def reset(self):
+		super().reset()
+		self.view_state = ""
+		self.view_state_generator = ""
+		self.event_validation = ""
 
-    def set_data(self, data: dict):
-        data["__VIEWSTATE"] = self.view_state
-        data["__VIEWSTATEGENERATOR"] = self.view_state_generator
-        data["__EVENTVALIDATION"] = self.event_validation
-        self.reset()
+	def set_data(self, data: dict):
+		data["__VIEWSTATE"] = self.view_state
+		data["__VIEWSTATEGENERATOR"] = self.view_state_generator
+		data["__EVENTVALIDATION"] = self.event_validation
+		self.reset()
 
 
